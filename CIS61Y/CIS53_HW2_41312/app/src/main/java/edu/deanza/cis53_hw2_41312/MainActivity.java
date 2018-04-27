4d338579c2de7c3348502c8a316b390539f77dcd
soundreaper <soundreaper@gmail.com>
2018-04-26 21:19:15 -0700

    Finish HW_2
    Finish HW_2: Use Android LogCat to observe the different methods and how
    they work and the logging methods that Android ueses.

--- CIS61Y/CIS53_HW2_41312/app/src/main/java/edu/deanza/cis53_hw2_41312/MainActivity.java
+++ CIS61Y/CIS53_HW2_41312/app/src/main/java/edu/deanza/cis53_hw2_41312/MainActivity.java
@@ -5,14 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String APP_TAG = "CIS53-HW2-41312";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(APP_TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -40,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        Log.v(APP_TAG, "onOptionsItemSelected Settings");
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -48,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.v(APP_TAG, "onPause");
     }
 }
